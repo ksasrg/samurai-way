5d14f39d93055b3b4b8a3564d5564bbcd2f9db9b
ksasrg <ksahtmlcss@gmail.com>
2023-01-31 12:32:38 +0600

    lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+
 
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="logo.webp" width={'5%'} alt="" />       
+      </header>
+      <nav className='nav'>
+        <div><a href="">Profile</a></div>
+        <div><a href="">Messages</a></div>
+        <div><a href="">News</a></div>
+        <div><a href="">Music</a></div>
+        <div><a href="">Settings</a></div>
+      </nav>
+      <div className='content'>
+        <div><img width={'100%'} src="https://www.thoughtco.com/thmb/EXpjUAx2ZEgV64eKcsAsX7Ucl6w=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages_482194715-56a1329e5f9b58b7d0bcf666.jpg" alt="" /></div>
+        <div><img src="" alt="" /></div>
+      </div>
     </div>
   );
 }

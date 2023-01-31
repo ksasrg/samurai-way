55b63009322aadffb61a363cb637febb1b2e36bd
ksasrg <ksahtmlcss@gmail.com>
2023-01-31 13:07:15 +0600

    lesson 12

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="logo.webp" width={'5%'} alt="" />
+    </header>
+  );
+};
+
+export default Header;
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div><img width={'100%'} src="https://www.thoughtco.com/thmb/EXpjUAx2ZEgV64eKcsAsX7Ucl6w=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages_482194715-56a1329e5f9b58b7d0bcf666.jpg" alt="" /></div>
+      <div>ava + description</div>
+      <div>My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;

eeac2645f131c87d837ff7a4963f8c885d54b808
ksasrg <ksahtmlcss@gmail.com>
2023-02-08 12:05:30 +0600

    20. NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
-      <div className={s.item}><a href='/profile'>Profile</a></div>
-      <div className={`${s.item} ${s.active}`}><a href='/messages'>Messages</a></div>
+      <div className={s.item}>
+        <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
+      </div>
+      <div className={s.item}>
+        <NavLink to='/messages' activeClassName={s.activeLink}>Messages</NavLink>
+      </div>
       <div className={s.item}><a href="">News</a></div>
       <div className={s.item}><a href="">Music</a></div>
       <div className={s.item}><a href="">Settings</a></div>

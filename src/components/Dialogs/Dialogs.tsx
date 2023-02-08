bb54e8a3bac0e8a40549d1034206ef0a2a0e2d58
ksasrg <ksahtmlcss@gmail.com>
2023-02-08 12:28:51 +0600

    21. Верстаем страницу Диалогов

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import s from './Dialogs.module.css'
 
-type DialogsPropsType ={
+type DialogsPropsType = {
 
 }
 
 const Dialogs = (props: DialogsPropsType) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+          ABC
+        </div>
+        <div className={s.dialog + ' ' + s.active}>
+          BCD
+        </div>
+        <div className={s.dialog}>
+          CDE
+        </div>
+        <div className={s.dialog}>
+          DEF
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>message 1</div>
+        <div className={s.message}>message 2</div>
+        <div className={s.message}>message 3</div>
+      </div>
     </div>
   );
 };

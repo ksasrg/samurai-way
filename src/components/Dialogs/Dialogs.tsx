9d7929043757bfe3e7e7ac0b5ee781572eab9cc1
ksasrg <ksahtmlcss@gmail.com>
2023-02-08 11:08:25 +0600

    18. страница Dialogs

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './Dialogs.module.css'
+
+type DialogsPropsType ={
+
+}
+
+const Dialogs = (props: DialogsPropsType) => {
+  return (
+    <div>
+      Dialogs
+    </div>
+  );
+};
+
+export default Dialogs;

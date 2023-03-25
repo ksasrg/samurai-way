1c4e3d4fc0e79fc35832cbae809259d6b973ec84
ksasrg <ksahtmlcss@gmail.com>
2023-03-25 09:40:22 +0600

    54  жизненный цикл, life cycle methods, componentDidMount

--- src/components/Users/UsersC.tsx
+++ src/components/Users/UsersC.tsx
@@ -3,55 +3,54 @@ import React, { useEffect } from 'react';
 import { UserType } from '../../redux/users-reducer';
 
 export type UsersPageType = {
-    users: UserType[]
-    follow: (userId: number) => void,
-    unfollow: (userId: number) => void,
-    setUsers: (users: UserType[]) => void,
+  users: UserType[]
+  follow: (userId: number) => void,
+  unfollow: (userId: number) => void,
+  setUsers: (users: UserType[]) => void,
 }
 
 
 export class Users extends React.Component<UsersPageType> {
-    constructor(props: UsersPageType) {
-        super(props)
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then((response) => this.props.setUsers(response.data.items))
-    }
+  componentDidMount(): void {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => this.props.setUsers(response.data.items))
+  }
 
-    render() {
-        return (
-            <div>
-                {
-                    this.props.users.map(u => {
-                        return (
-                            <div key={u.id}>
-                                <span>
-                                    <div>
-                                        <img src={u.photos.small ? u.photos.small : "avatar.webp"} style={{ width: '50px' }} />
-                                    </div>
-                                    <div>
-                                        {u.followed
-                                            ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
-                                            : <button onClick={() => this.props.follow(u.id)}>Follow</button>
-                                        }
+  render() {
+    return (
+      <div>
+        {
+          this.props.users.map(u => {
+            return (
+              <div key={u.id}>
+                <span>
+                  <div>
+                    <img src={u.photos.small ? u.photos.small : "avatar.webp"} style={{ width: '50px' }} />
+                  </div>
+                  <div>
+                    {u.followed
+                      ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                      : <button onClick={() => this.props.follow(u.id)}>Follow</button>
+                    }
 
-                                    </div>
-                                </span>
-                                <span>
-                                    <span>
-                                        <div>{u.name}</div>
-                                        <div>{u.status}</div>
-                                    </span>
-                                    <span>
-                                        {/* <div>{u.location.country}</div>
+                  </div>
+                </span>
+                <span>
+                  <span>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
+                  </span>
+                  <span>
+                    {/* <div>{u.location.country}</div>
                                         <div>{u.location.city}</div> */}
-                                    </span>
-                                </span>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        );
-    }
+                  </span>
+                </span>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
